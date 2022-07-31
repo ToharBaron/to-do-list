834c682c94ff8f3ef5b37c22841daae9ce423b98
Tohar Bar-on <toahr2t2@gmail.com>
2022-07-31 16:13:02 +0300

    add header + footer ejs tamplates

--- app.js
+++ app.js
@@ -4,41 +4,69 @@ const bodyParser = require("body-parser");
 const app = express();
 
 let items = [];
+let workItem = [];
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
-app.get("/", function(req, res){
-let today = new Date();
+app.get("/", function(req, res) {
+  let today = new Date();
 
-let options ={
-  weekday:'long',
-  day:'numeric',
-  month: 'long'
-};
+  let options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long'
+  };
 
-let day = today.toLocaleDateString('en-us', options);
+  let day = today.toLocaleDateString('en-us', options);
 
 
-res.render('list', {
-  kindOfDay:day,
-  newListItems:items
+  res.render('list', {
+    listTitle: day,
+    newListItems: items
+  });
+
 });
 
-});
-
-app.post("/",function(req, res){
+app.post("/", function(req, res) {
   let item = req.body.newItem;
-  if (item === ""){
-    res.redirect("/");
-  } else{
-  items.push(item);
-  res.redirect("/");
-}
+  console.log(req.body);
+  if (req.body.list === "Work List") {
+    if (req.body.newItem === "") {
+      res.redirect("/work");
+    } else {
+      workItem.push(item);
+      res.redirect("/work");
+    }
+  } else {
+    if (req.body.newItem === "") {
+      res.redirect("/");
+    } else {
+      items.push(item);
+      res.redirect("/");
+    }
+  }
 });
 
-app.listen(3000, function(){
+app.get("/work", (req, res) => {
+  res.render('list', {
+    listTitle: "Work List",
+    newListItems: workItem
+  });
+});
+
+app.post("/work", function(req, res) {
+  let item = req.body.newItem;
+
+  workItem.push(item);
+  res.redirect("/work");
+});
+
+
+app.listen(3000, function() {
   console.log("Server started on port 3000.");
 });
